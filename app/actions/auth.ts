35e536bbcae1ec6db92a6327ade20fc6f692b1ad
Kuldeep Saini <k72125642@gmail.com>
2025-12-28 19:38:17 +0530

    add cookie session

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -3,20 +3,29 @@
 import axios from "axios"
 import { redirect } from "next/navigation"
 import { UserType } from "../types/user"
+import { deletesession, setSession } from "../_lib/session"
 
 
 const API_URL = "http://localhost:3001"
 
-export const login = async (formData: FormData) => {
-   try {
-    const response = await axios.get(`${API_URL}/users?email=${formData.get("email")}&password=${formData.get("password")}`
+export const loginAction = async (formData: FormData) => {
+    try {
+    const response = await axios.get(
+      `${API_URL}/users?email=${formData.get("email")}&password=${formData.get("password"
+      )}`
   )
     const user: UserType = response.data[0];
+    if(!user) throw new Error("Invalid Credentials")
+      // set use in the cookies
+    await setSession({ name: user.name, email: user.email, id: user.id })
    } catch (error) {
-
+    console.error("Login error:", error);
+   throw new Error ("Failed to login");
    } 
+   redirect("/contact")
 }
 
-export const logout = () => {
+export const logoutAction = async () => {
+  await deletesession()
     redirect("/login")
 }
